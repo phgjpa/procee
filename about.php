44395e358bc33d89c0834516f6e2d3524cea4b1a
phgjpa <phgjpa@github.com>
2021-01-15 11:03:02 +0800

    updated about.php

--- about.php
+++ about.php
@@ -58,7 +58,6 @@
           <div class="row">
             <div class="column">
               <div class="card">
-                <img src="/resources/images/person1.png" alt="Jane" style="width:100%">
                 <div class="container">
                   <h2>Jane Doe</h2>
                   <p class="title">CEO & Founder</p>
@@ -71,7 +70,6 @@
 
             <div class="column">
               <div class="card">
-                <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
                 <div class="container">
                   <h2>Mike Ross</h2>
                   <p class="title">Art Director</p>
@@ -84,7 +82,6 @@
 
             <div class="column">
               <div class="card">
-                <img src="/w3images/team3.jpg" alt="John" style="width:100%">
                 <div class="container">
                   <h2>John Doe</h2>
                   <p class="title">Designer</p>
