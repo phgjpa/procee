b757da2d1e2ae1214e668afdbdc590de2c1067a3
KER4 <itsuka.kokonoe@gmail.com>
2021-01-15 09:22:57 +0800

    Update contact us

--- contact_us.php
+++ contact_us.php
@@ -52,7 +52,7 @@
           <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
             <h1 class="display-4 py-2 text-truncate">Contact Us</h1>
             <div class="px-2">
-              <form action="" class="justify-content-center">
+              <form action="" class="justify-content-center"> 
                 <div class="form-group">
                   <label class="sr-only">Name :</label>
                   <input type="text" class="form-control" placeholder="Your Name">
